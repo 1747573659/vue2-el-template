cb718570977a9d87a3a6137678cbea3dd805af47
haochen <2390154725@qq.com>
2020-12-08 14:03:43 +0800

    feat: 删除商户管理placeholder

--- src/pages/customer/merchant/merchantManage/addMerchant.vue
+++ src/pages/customer/merchant/merchantManage/addMerchant.vue
@@ -61,7 +61,7 @@
             <div class="com-edit-block">
               <div class="com-edit-ruleForm__content">
                 <el-form-item label="品牌名称：" prop="merchantName">
-                  <el-input v-model="ruleForm.merchantName" maxlength="30" placeholder="商户名称需与营业执照一致"></el-input>
+                  <el-input v-model="ruleForm.merchantName" maxlength="30"></el-input>
                 </el-form-item>
                 <el-form-item label="品牌LOGO：">
                   <pic-upload :uploadUrl="uploadUrl" :imageUrl="ruleForm.logo" :fileServer="ossFileServe" :showIconClose="true" @on-remove="onRemove" @on-success="onUploadSuccess">
