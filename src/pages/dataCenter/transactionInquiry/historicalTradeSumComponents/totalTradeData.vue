c1a1c988f823964f92aec59e0bc7f7001ef79026
chennanfang <chennanfang@kemai.cn>
2020-12-17 16:46:06 +0800

    解决select接口重复掉二遍问题

--- src/pages/dataCenter/transactionInquiry/historicalTradeSumComponents/totalTradeData.vue
+++ src/pages/dataCenter/transactionInquiry/historicalTradeSumComponents/totalTradeData.vue
@@ -3,18 +3,9 @@
     <div class="search-box">
       <div class="xdd_tip">
         <i class="el-icon-info"></i>
-          查询对象为“全部” 或 代理商过滤时，结果会包括下级代理商的交易数据
+        查询对象为“全部” 或 代理商过滤时，结果会包括下级代理商的交易数据
       </div>
-      <el-form
-        :inline="true"
-        :model="form"
-        label-suffix=":"
-        @submit.native.prevent
-        label-width="80px"
-        ref="form"
-        size="small"
-        class="xdd-btn-block__w240"
-      >
+      <el-form :inline="true" :model="form" label-suffix=":" @submit.native.prevent label-width="80px" ref="form" size="small" class="xdd-btn-block__w240">
         <el-row>
           <el-col :span="21">
             <el-form-item label="查询对象" prop="paymentCode">
@@ -27,7 +18,7 @@
                 :optionsItem="{
                   key: 'id',
                   label: 'name',
-                  value: 'id',
+                  value: 'id'
                 }"
               ></selectCopy>
             </el-form-item>
@@ -63,13 +54,7 @@
               </el-select>
             </el-form-item>
             <el-form-item label="" prop="paymentCode">
-              <el-button
-                type="primary"
-                class="km-archive-search"
-                :loading="cxLoading"
-                @click="getList"
-                >查询</el-button
-              >
+              <el-button type="primary" class="km-archive-search" :loading="cxLoading" @click="getList">查询</el-button>
               <!-- <el-button class="km-archive-search" :loading="cxLoading" @click="getList">导出</el-button>
               <el-button class="km-archive-search" :loading="cxLoading" @click="getList">导出记录</el-button> -->
             </el-form-item>
@@ -85,9 +70,7 @@
             <div class="sum-card-title">
               交易总额(元)
               <el-tooltip effect="dark" placement="top">
-                <div slot="content">
-                  当前过滤条件的交易总额<br />(未扣除退款订单金额)
-                </div>
+                <div slot="content">当前过滤条件的交易总额<br />(未扣除退款订单金额)</div>
                 <img :src="questionIcon" alt="提示" class="e-icon-question" />
               </el-tooltip>
             </div>
@@ -99,9 +82,7 @@
             <div class="sum-card-title">
               交易笔数
               <el-tooltip effect="dark" placement="top">
-                <div slot="content">
-                  当前过滤条件的交易笔数<br />(未扣除退款订单笔数)
-                </div>
+                <div slot="content">当前过滤条件的交易笔数<br />(未扣除退款订单笔数)</div>
                 <img :src="questionIcon" alt="提示" class="e-icon-question" />
               </el-tooltip>
             </div>
@@ -126,13 +107,13 @@
               商户优惠(元)
               <el-tooltip effect="dark" placement="top">
                 <div slot="content">
-                  由商家承担的参与微信/支付宝/银联<br>
+                  由商家承担的参与微信/支付宝/银联<br />
                   活动的优惠券核销金额
                 </div>
                 <img :src="questionIcon" alt="提示" class="e-icon-question" />
               </el-tooltip>
             </div>
-            <div class="sum-card-money">{{tableData.shopCouponAmount}}</div>
+            <div class="sum-card-money">{{ tableData.shopCouponAmount }}</div>
           </div>
         </el-col>
         <el-col :span="4" class="sum-card-item">
@@ -141,13 +122,13 @@
               商户实退(元)
               <el-tooltip effect="dark" placement="top">
                 <div slot="content">
-                  退还用户银行卡或零钱账户的<br>
+                  退还用户银行卡或零钱账户的<br />
                   金额+平台优惠券退款金额
                 </div>
                 <img :src="questionIcon" alt="提示" class="e-icon-question" />
               </el-tooltip>
             </div>
-            <div class="sum-card-money">{{tableData.merchantRefundAmount}}</div>
+            <div class="sum-card-money">{{ tableData.merchantRefundAmount }}</div>
           </div>
         </el-col>
         <el-col :span="4" class="sum-card-item">
@@ -159,7 +140,7 @@
                 <img :src="questionIcon" alt="提示" class="e-icon-question" />
               </el-tooltip>
             </div>
-            <div class="sum-card-money">{{tableData.receiptAmount}}</div>
+            <div class="sum-card-money">{{ tableData.receiptAmount }}</div>
           </div>
         </el-col>
       </el-row>
@@ -168,48 +149,41 @@
 </template>
 
 <script>
-import moment from "moment";
-import { getLocal } from "@/utils/storage";
-import selectPage from "@/components/selectPage";
-import selectCopy from "@/components/selectCopy";
-import {
-  paymentMethodVoList,
-  cashierData,
-  queryNewAgentPage,
-  queryShopListByPage,
-  queryStorePage,
-  paymentPluginVoList
-} from "@/api/dataCenter/historiyTrade";
+import moment from 'moment'
+import { getLocal } from '@/utils/storage'
+import selectPage from '@/components/selectPage'
+import selectCopy from '@/components/selectCopy'
+import { paymentMethodVoList, cashierData, queryNewAgentPage, queryShopListByPage, queryStorePage, paymentPluginVoList } from '@/api/dataCenter/historiyTrade'
 
 export default {
-  name: "monthTradeData",
+  name: 'monthTradeData',
   components: {
     selectPage,
-    selectCopy,
+    selectCopy
   },
   data() {
     return {
       selectPageNo: 1,
-      activeIndex: "1",
+      activeIndex: '1',
       isMaxPage: false,
-      selectPageName: "",
+      selectPageName: '',
       form: {
         time: [],
-        payMethod: "",
-        searchObject: "",
-        ObjContent: "",
+        payMethod: '',
+        searchObject: '',
+        ObjContent: '',
         payPlugin: ''
       },
-      payPluginList: [{id: '', name: '全部'}],
+      payPluginList: [{ id: '', name: '全部' }],
       paymentList: [],
-      questionIcon: require("@/assets/images/icon/questioin.png"),
+      questionIcon: require('@/assets/images/icon/questioin.png'),
       ObjContentList: [],
       tableData: {},
       searchObjectList: [
-        { id: "", name: "全部" },
-        { id: 1, name: "代理商" },
-        { id: 2, name: "商户" },
-        { id: 3, name: "门店" },
+        { id: '', name: '全部' },
+        { id: 1, name: '代理商' },
+        { id: 2, name: '商户' },
+        { id: 3, name: '门店' }
       ],
       tableLoading: false,
       currentPage: 1,
@@ -217,32 +191,32 @@ export default {
       totalPage: 0,
       eChartsDateList: [],
       eChartsDataList: [],
-      cxLoading: false,
-    };
+      cxLoading: false
+    }
   },
   computed: {
     tableMaxHeight() {
-      return document.documentElement.clientHeight - 56 - 48 - 64 - 32 - 210;
+      return document.documentElement.clientHeight - 56 - 48 - 64 - 32 - 210
     },
     isSalesman() {
-      return Boolean(JSON.parse(getLocal("userInfo")).clerkId);
-    },
+      return Boolean(JSON.parse(getLocal('userInfo')).clerkId)
+    }
   },
   mounted() {
-    this.getList();
-    this.getpaymentMethodVoList();
+    this.getList()
+    this.getpaymentMethodVoList()
   },
   created() {
     if (this.isSalesman) {
       this.searchObjectList = [
-        { id: "", name: "全部" },
-        { id: 2, name: "商户" },
-        { id: 3, name: "门店" },
-      ];
+        { id: '', name: '全部' },
+        { id: 2, name: '商户' },
+        { id: 3, name: '门店' }
+      ]
     }
   },
   methods: {
-    async payMethodChange (value) {
+    async payMethodChange(value) {
       let childs = ''
       console.log(value)
       this.paymentList.forEach(item => {
@@ -256,125 +230,128 @@ export default {
       try {
         let res = await paymentPluginVoList(data)
         this.payPluginList = res
-        this.payPluginList.unshift({id: '', name: '全部'})
+        this.payPluginList.unshift({ id: '', name: '全部' })
       } catch (error) {}
     },
     searchObjectChange(value) {
-      this.form.id = null;
-      this.form.ObjContent = null;
-      this.isMaxPage = false;
-      this.ObjContentList = [];
-      this.searchString = "";
-      this.selectPageNo = 1;
+      this.form.id = null
+      this.form.ObjContent = null
+      this.isMaxPage = false
+      this.ObjContentList = []
+      this.searchString = ''
+      this.selectPageNo = 1
     },
     async remoteMethod(value) {
       if (!this.form.searchObject) {
-        this.$message.warning("请先选择查询对象");
-        return;
+        this.$message.warning('请先选择查询对象')
+        return
       }
       // 当输入新的值的时候，就把相关数据进行情况
       if (value !== this.searchString) {
-        this.selectPageNo = 1;
-        this.searchString = "";
-        this.isMaxPage = false;
-        this.ObjContentList = [];
+        this.selectPageNo = 1
+        this.searchString = ''
+        this.isMaxPage = false
+        this.ObjContentList = []
       }
       // 只有value有值的时候才去请求接口
       let data = {
-        id: value || "",
+        id: value || '',
         page: this.selectPageNo,
-        rows: 10,
-      };
+        rows: 10
+      }
       try {
-        let res;
+        let res
         if (this.form.searchObject === 1) {
-          res = await queryNewAgentPage(data);
-          this.selectPageName = "name";
+          res = await queryNewAgentPage(data)
+          this.selectPageName = 'name'
         } else if (this.form.searchObject === 2) {
-          res = await queryShopListByPage(data);
-          this.selectPageName = "companyName";
+          res = await queryShopListByPage(data)
+          this.selectPageName = 'companyName'
         } else if (this.form.searchObject === 3) {
-          res = await queryStorePage(data);
-          this.selectPageName = "name";
+          res = await queryStorePage(data)
+          this.selectPageName = 'name'
         }
         // 如果分页返回有数据，就将数据加入list，如果接口返回数据长度不为10，则说明为最后一页
         if (res.results && res.results.length !== 0) {
-          this.ObjContentList = this.ObjContentList.concat(res.results);
-          this.searchString = value;
+          this.ObjContentList = this.ObjContentList.concat(res.results)
+          this.searchString = value
           if (res.results?.length !== 10) {
-            this.isMaxPage = true;
+            this.isMaxPage = true
           }
         } else {
-          this.isMaxPage = true;
+          this.isMaxPage = true
         }
       } catch (error) {}
     },
     loadMore() {
       // 如果不是最后一页就加载下一页
       if (!this.isMaxPage) {
-        this.selectPageNo++;
-        this.remoteMethod(this.searchString);
+        this.selectPageNo++
+        this.remoteMethod(this.searchString)
       }
     },
     selectPageFocus() {
-      this.isMaxPage = false;
-      this.ObjContentList = [];
-      this.searchString = "";
-      this.selectPageNo = 1;
-      this.remoteMethod();
+      this.isMaxPage = false
+      this.ObjContentList = []
+      this.searchString = ''
+      this.selectPageNo = 1
+      if (!this.form.ObjContent) {
+        this.remoteMethod()
+      }
     },
     selectPageChange(value) {
-      this.form.id = value;
-      this.ObjContentList.forEach((item) => {
+      this.form.id = value
+      this.ObjContentList.forEach(item => {
         if (item.id === value) {
           if (this.form.searchObject === 2) {
-            this.form.ObjContent = item.companyName;
+            this.form.ObjContent = item.companyName
           } else {
-            this.form.ObjContent = item.name;
+            this.form.ObjContent = item.name
           }
         }
-      });
+      })
     },
     // 如果点击了清除按钮则将相关数据清空
     selectPageClear() {
-      this.isMaxPage = false;
-      this.ObjContentList = [];
-      this.searchString = "";
-      this.selectPageNo = 1;
+      this.isMaxPage = false
+      this.ObjContentList = []
+      this.searchString = ''
+      this.selectPageNo = 1
+      this.form.ObjContent = ''
     },
     handleCurrentChange() {},
     handleSizeChange() {},
     handleSelect(key, keyPath) {
-      this.activeIndex = String(key);
+      this.activeIndex = String(key)
     },
     setSearchTime(type) {},
     async getpaymentMethodVoList() {
       try {
-        const res = await paymentMethodVoList();
-        this.paymentList = res;
-        this.paymentList.unshift({ id: "", name: "全部" });
+        const res = await paymentMethodVoList()
+        this.paymentList = res
+        this.paymentList.unshift({ id: '', name: '全部' })
       } catch (error) {}
     },
     async getList() {
-      this.tableLoading = true;
+      this.tableLoading = true
       let data = {
         type: 4,
-        adminId: this.form.searchObject === 2 ? this.form.id : "",
-        agentId: this.form.searchObject === 1 ? this.form.id : "",
-        storeId: this.form.searchObject === 3 ? this.form.id : "",
+        adminId: this.form.searchObject === 2 ? this.form.id : '',
+        agentId: this.form.searchObject === 1 ? this.form.id : '',
+        storeId: this.form.searchObject === 3 ? this.form.id : '',
         payMethod: this.form.payMethod,
         payPlugin: this.form.payPlugin
-      };
+      }
       try {
-        const res = await cashierData(data);
-        this.tableData = res;
+        const res = await cashierData(data)
+        this.tableData = res
       } catch (error) {
       } finally {
-        this.tableLoading = false;
+        this.tableLoading = false
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
