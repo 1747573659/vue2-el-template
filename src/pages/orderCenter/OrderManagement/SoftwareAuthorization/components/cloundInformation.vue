e1ff4789530b1d46644a693343c10f6fba68a6d8
liuhaofeng <liuhaofeng@kemai.cn>
2021-12-07 19:13:11 +0800

    fix: 小蜜有批类型添加

--- src/pages/orderCenter/OrderManagement/SoftwareAuthorization/components/cloundInformation.vue
+++ src/pages/orderCenter/OrderManagement/SoftwareAuthorization/components/cloundInformation.vue
@@ -393,7 +393,7 @@ export default {
         this.checkProductTabLock = true
         this.currentPageSelectSets.clear()
         const data = { PageSize: 100, PageIndex: 0, flag: -1, condition: this.productVal, CustId: this.form.merchantDTO.merchantNo, AppId: this.appModuleObj.outCode }
-        // if (this.appModuleObj?.code === 206) data.xmyp = 1
+        if (this.appModuleObj?.code === 206) data.xmyp = 1
         const res = await authOrderYsByCusAndApplyList(data)
         this.basicProductData = res
         this.$nextTick(() => {
