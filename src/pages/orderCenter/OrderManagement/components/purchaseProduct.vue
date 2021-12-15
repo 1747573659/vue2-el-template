a4edcc48ac02699e3f9472ef35dea98c9f3ac25e
liuhaofeng <liuhaofeng@kemai.cn>
2021-12-15 09:58:57 +0800

    feat: 采购订单弹窗选择修改

--- src/pages/orderCenter/OrderManagement/components/purchaseProduct.vue
+++ src/pages/orderCenter/OrderManagement/components/purchaseProduct.vue
@@ -6,7 +6,7 @@
       </el-form-item>
       <el-button type="primary" size="small" @click="getProductPage">查询</el-button>
     </el-form>
-    <el-table ref="product" :data="basicProductData" v-loading="checkProductTabLock">
+    <el-table ref="product" :data="basicProductData" @select="handleSelect" @select-all="handleSelectAll" v-loading="checkProductTabLock">
       <el-table-column type="selection" width="55"></el-table-column>
       <el-table-column prop="code" label="产品编码"></el-table-column>
       <el-table-column prop="name" label="产品名称"></el-table-column>
@@ -31,13 +31,36 @@ export default {
       basicProductData: [],
       currentPage: 1,
       pageSize: 10,
-      totalPage: 0
+      totalPage: 0,
+      selectMaps: new Map(),
+      currentPageSelectSets: new Set()
     }
   },
   methods: {
+    handleSelectAll(selection) {
+      if (selection?.length) {
+        selection.forEach(item => {
+          this.selectMaps.set(item.code, item)
+          this.currentPageSelectSets.add(item.code)
+        })
+      } else {
+        this.currentPageSelectSets.forEach(item => this.selectMaps.delete(item))
+        this.currentPageSelectSets.clear()
+      }
+    },
+    handleSelect(selection, row) {
+      if (selection.length > this.currentPageSelectSets.size) {
+        this.selectMaps.set(row.code, row)
+        this.currentPageSelectSets.add(row.code)
+      } else {
+        this.selectMaps.delete(row.code)
+        this.currentPageSelectSets.delete(row.code)
+      }
+    },
     handleConfirm() {
-      const Selections = this.$refs.product.selection.map(item => {
-        return {
+      let Selections = []
+      this.selectMaps.forEach(item => {
+        Selections.push({
           productCode: item.code,
           productName: item.name,
           productCount: 1,
@@ -45,10 +68,12 @@ export default {
           productAmount: NP.times(1, item.saleAmount),
           sourcePrice: NP.times(1, item.saleAmount),
           remark: ''
-        }
+        })
       })
       this.$emit('productData', Selections)
       this.$emit('update:visible', false)
+      this.currentPageSelectSets.clear()
+      this.selectMaps.clear()
     },
     handleProductClose() {
       this.currentPage = 1
@@ -57,14 +82,25 @@ export default {
     getProductPage: async function() {
       try {
         this.checkProductTabLock = true
-        const { results = [], totalRecord = 0 } = await productInfo({
+        this.currentPageSelectSets.clear()
+        const res = await productInfo({
           info: this.productVal.trim(),
           page: this.currentPage,
           rows: this.pageSize,
           orderType: this.$route.name === 'hardwarePurchaseDetails' ? 0 : 1
         })
-        this.basicProductData = results
-        this.totalPage = totalRecord
+        this.basicProductData = res?.results ?? []
+        this.totalPage = res.totalRecord ?? 0
+        this.$nextTick(() => {
+          if (this.basicProductData?.length) {
+            this.basicProductData.forEach(item => {
+              if (this.selectMaps?.size && this.selectMaps.has(item.code)) {
+                this.currentPageSelectSets.add(item.code)
+                this.$refs.product.toggleRowSelection(item, true)
+              }
+            })
+          }
+        })
       } catch (error) {
       } finally {
         this.checkProductTabLock = false
