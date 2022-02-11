dc4c369efe51e0592354c0d2e82cfe95551a1165
chennanfang <chennanfang@kemai.cn>
2022-02-11 16:56:27 +0800

    1.软件库存申请单列表申请经销商加上经销商编码 2.编辑和详情界面加上申请，数据取的也不对

--- src/pages/orderCenter/OrderManagement/SoftwareInventoryApply/components/basicInformation.vue
+++ src/pages/orderCenter/OrderManagement/SoftwareInventoryApply/components/basicInformation.vue
@@ -26,7 +26,7 @@
           <el-input :value="form.orderDTO.handlerUserName"></el-input>
         </el-form-item>
         <el-form-item label="经销商">
-          <el-input disabled :value="`${userInfo.agentId ? '[' + userInfo.agentId + ']' : ''}${userInfo.name}`"></el-input>
+          <el-input disabled :value="`${form.orderDTO.agentId ? '[' + form.orderDTO.agentId + ']' : ''}${form.orderDTO.agentName}`"></el-input>
         </el-form-item>
       </el-form>
     </el-card>
