1c7f34ddfddb78f0b470aaf97d2a8b5442adedcc
liuhaofeng <liuhaofeng@kemai.cn>
2022-02-12 18:15:07 +0800

    fix: erp授权单页签修复

--- src/pages/orderCenter/OrderManagement/ErpAuthorizedTransfer/erpAuthorizedTransferDetails.vue
+++ src/pages/orderCenter/OrderManagement/ErpAuthorizedTransfer/erpAuthorizedTransferDetails.vue
@@ -32,7 +32,7 @@ export default {
   mounted() {
     this.$nextTick(() => {
       const { orderStatus: orderStatusVal } = this.$route.query
-      document.querySelector('.e-tag_active span').innerText = `erp授权转移单/${orderStatus.has(orderStatusVal) ? orderStatus.get(orderStatusVal).name : '新增'}`
+      document.querySelector('.e-tag_active span').innerText = `ERP授权转移单/${orderStatus.has(orderStatusVal) ? orderStatus.get(orderStatusVal).name : '新增'}`
     })
   },
   methods: {
