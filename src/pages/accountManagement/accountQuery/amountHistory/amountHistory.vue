d63d6f6e035db8758f7ea3c06d9e7c8bade611e6
chennanfang <chennanfang@kemai.cn>
2022-02-16 10:09:39 +0800

    页面开发

--- src/pages/accountManagement/accountQuery/amountHistory/amountHistory.vue
+++ src/pages/accountManagement/accountQuery/amountHistory/amountHistory.vue
@@ -1,3 +1,27 @@
 <template>
-    <div>余额变动流水</div>
+  <div>
+    <div class="search-box">
+      <el-row>
+        <el-col :span="20">
+          1
+          <!-- <el-form :inline="true" size="small" :model="form" label-width="85px" class="xdd-btn-block__w240">
+            <el-form-item label="商户信息：">
+              <el-input v-model="form.merchantAdminName" maxlength="50" placeholder="请输入商户编号/名称" clearable></el-input>
+            </el-form-item>
+            <el-form-item label="品牌信息：">
+              <el-input v-model="form.merchantName" maxlength="50" placeholder="请输入品牌编号/名称" clearable></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="getPageList">查询</el-button>
+            </el-form-item>
+          </el-form> -->
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+
+}
+</script>
