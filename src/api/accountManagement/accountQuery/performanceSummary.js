0425611d73a0a680e34ad8a7dbf62c32d70cfca7
haochen <2390154725@qq.com>
2022-03-16 15:24:32 +0800

    feat: axios 绝对url删除

--- src/api/accountManagement/accountQuery/performanceSummary.js
+++ src/api/accountManagement/accountQuery/performanceSummary.js
@@ -1,12 +1,9 @@
 import request from '@/utils/request'
 
-// import axios from 'axios'
-// axios.config.baseURL = 'http://127.0.0.1:4523/mock/526932'
-
 // 分页列表
 export function queryByPage(data) {
   return request({
-    url: 'http://127.0.0.1:4523/mock/526932/performance/summary/page',
+    url: '/performance/summary/page',
     method: 'POST',
     data
   })
