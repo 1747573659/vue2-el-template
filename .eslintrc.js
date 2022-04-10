e765a3cf34e5306a97192798b666e00dfd40851a
haochen <2390154725@qq.com>
2022-04-10 17:47:04 +0800

    feat: init format

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@babel/eslint-parser'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" && process.env.VUE_APP_FLAG === "pro" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production"  && process.env.VUE_APP_FLAG === "pro" ? "warn" : "off",
-    "no-unused-vars": "off", // 关闭未使用变量验证
-    "no-empty": 'off' // 关闭块语句中的内容不能为空
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error'
   }
-};
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+
 Vue.use(Vuex)
 
-// 载入modules下所有状态管理器文件
-const files = require.context('./modules', false, /\.js$/)
-const modules = {}
-
-files.keys().forEach(key => {
-  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
-
 export default new Vuex.Store({
-  modules
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
