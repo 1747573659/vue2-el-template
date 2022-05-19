7099e1eaa78f9a04d25df0d01c577a3f602ba96d
pengliming <lm881993@163.com>
2022-05-19 09:20:40 +0800

    feat: [YS-11964]有数商户管理&&有数商户详情查看

---
+++ src/pages/customer/merchant/ysMerchantManagement/data.js
@@ -0,0 +1,40 @@
+export const tipsData = [
+  { status: 0, label: '即将到期', total: 0 },
+  { status: 1, label: '付费使用中', total: 0 },
+  { status: 3, label: '试用中', total: 0 },
+  { status: 2, label: '已到期', total: 0 }
+]
+// 账号状态
+export const accountStatusData = [
+  { value: 0, label: '未试用' },
+  { value: 1, label: '试用中' },
+  { value: 2, label: '试用到期' },
+  { value: 3, label: '付费使用中' },
+  { value: 4, label: '即将到期（不足30天）' },
+  { value: 5, label: '已到期' }
+]
+// erp产品类型
+export const erpSelectList = [
+  { label: '御商+', value: 'ADV10' },
+  { label: '御商V9', value: 'ADV9' },
+  { label: '云鼎', value: 'YD' },
+  { label: '智海鲸', value: 'BLDPRO' },
+  { label: '智赢+', value: 'ZY10' },
+  { label: '御云', value: 'YY01' },
+  { label: '御鼎', value: 'YD01' }
+]
+
+export const baseInfoItems = [
+  { title: '客户名称', value: '' },
+  { title: '服务到期时间', value: '' },
+  { title: '渠道来源', value: '' },
+  { title: '客户地址', value: '' },
+  { title: 'ERP客户编码', value: '' },
+  { title: '微平台商户ID', value: '' }
+]
+export const versionsItems = [
+  { title: '版本名称', value: '' },
+  { title: '期限至', value: '' },
+  { title: '购买状态', value: '' },
+  { title: '版本开通费用', value: '' }
+]
